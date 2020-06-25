22667312080b17dca74acc92f41922a28a5b7782
Himanshu Maurya <himanshumaurya2214225@gmail.com>
2020-06-25 19:42:36 +0530

    fixed some bug in starter code

--- consumers/topic_check.py
+++ consumers/topic_check.py
@@ -5,7 +5,7 @@ def topic_exists(topic):
     """Checks if the given topic exists in Kafka"""
     client = AdminClient({"bootstrap.servers": "PLAINTEXT://localhost:9092"})
     topic_metadata = client.list_topics(timeout=5)
-    return topics in topic_metadata.topics
+    return topic in topic_metadata.topics
 
 def contains_substring(to_test, substr):
     _before, match, _after = to_test.partition(substr)
@@ -17,7 +17,8 @@ def topic_pattern_match(pattern):
         Returns `True` if one or more topic names contains substring `pattern`.
         Returns `False` if not.
     """
-    topic_metadata = client.list_topics()
+    client = AdminClient({"bootstrap.servers": "PLAINTEXT://localhost:9092"})
+    topic_metadata = client.list_topics(timeout=5)
     topics = topic_metadata.topics
     filtered_topics = {key: value for key, value in topics.items() if contains_substring(key, pattern)}
     return len(filtered_topics) > 0
